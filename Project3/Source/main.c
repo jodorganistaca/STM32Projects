883105ff615e8f3e3243692104a445e1a60a9b7e
jodorganistaca <jodorganistaca@unal.edu.co>
2022-06-07 14:23:34 +0200

    adc

--- Project3/Source/main.c
+++ Project3/Source/main.c
@@ -2,17 +2,17 @@
 #include "Driver_Timer.h"
 #include "Driver_GPIO.h"
 
-int voltage_poten;
+int voltage_poten = 0;
 
 void Callback(void){
-
-	printf("%d",voltage_poten);
+	voltage_poten = (int) ADC1->DR / 10;
+	//printf("%d",voltage_poten);
 }
 
 int main(void){
 	
 	
-	MyGPIO_Struct_TypeDef a;
+	MyGPIO_Struct_TypeDef a, led;
 	MyTimer_Struct_TypeDef t;
 	
 	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
@@ -26,17 +26,27 @@ int main(void){
 	a.GPIO_Conf = In_Analog;
 	a.GPIO_Pin = 0;
 	
+	led.GPIO = GPIOA;
+	led.GPIO_Conf = Out_PushPull;
+	led.GPIO_Pin = 1;
+	
 	t.Timer = TIM1;
 	t.ARR = 7200;
 	t.PSC = 10000;
 	
 	MyGPIO_Init(&a);	
+	MyGPIO_Init(&led);	
 	MyTimer_Base_Init(&t);
 	t.Timer->CR1 |= TIM_CR1_CEN;
-	MyTimer_ActiveIT(t.Timer,4,Callback);
+	MyTimer_ActiveIT(t.Timer,1,Callback);
 	ADC1->CR2 |= ADC_CR2_ADON;
 
  while(1){
 	voltage_poten = (int) ADC1->DR / 10;
+	 if(voltage_poten > 50){
+		MyGPIO_Set(led.GPIO,led.GPIO_Pin);
+	 }else{
+		MyGPIO_Reset(led.GPIO,led.GPIO_Pin);
+	 }
  };
 }
